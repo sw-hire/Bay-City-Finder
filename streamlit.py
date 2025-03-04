fa2c385e50e87d51167397f83ae0f9ce3f488bbe
sw-hire <sw.hiremani@gmail.com>
2025-03-03 20:31:57 -0800

    Fixed commit time issue Transit label

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,7 @@ from os import access
 import streamlit as st
 from filternew import findcity
 
-st.title("Bay Area City Finder")
+st.title("Bay Area City Finder 🌉")
 
 st.markdown("Welcome to the Bay Area City Finder!"
             " Picking the right place to live in the Bay Area is tough—prices are high, commutes are long, and every city is different. This tool makes it easier!"
@@ -12,24 +12,34 @@ st.markdown("Welcome to the Bay Area City Finder!"
 
 st.text("Alright, let's get started!")
 
+meetsCondition = True
+
 budget = st.number_input("What is your budget for a home ($)?")
 if budget < 453000.00 and budget > 0:
-    st.text("Sorry! Median house prices as of 2021 start at 453000."
+    meetsCondition = False
+    st.error("Sorry! Median house prices as of 2021 start at 453000."
             " Please enter a value at or above 453000.")
 
 mode = st.selectbox("How do you prefer to commute?",
-             ("", "Public Transit", "Carpool", "Drive Alone", "Bicycle", "Walk", "Work From Home"))
+             ("", "Transit", "Carpool", "Drive Alone", "Bicycle", "Walk", "Work From Home"))
 
 time = st.number_input("What is your ideal commute time?")
 if time < 17.34 and time > 0:
-    st.text("Sorry! Average commute times as of 2018 start at 17.34 minutes. Please enter a value at or above 17.34.")
-st.text("Please rank your budget, mode of commute, and ideal commute time.")
+    meetsCondition = False
+    st.error("Sorry! Average commute times as of 2018 start at 17.34 minutes. Please enter a value at or above 17.34.")
+st.text("In order of importance, please rank your top 2 among your budget, mode of commute, and ideal commute time.")
 pref1 = st.selectbox("First Ranking:",
                     ("Budget", "Mode", "Commute Time"))
 pref2 = st.selectbox("Second Ranking:",
                     ("Budget", "Mode", "Commute Time"))
 if pref1 == pref2:
-    st.text("Sorry! Please select a different second ranking from your first choice.")
+    meetsCondition = False
+    st.error("Please select a different second ranking from your first choice.")
 
-if st.button("Find My Cities"):
-    st.dataframe(findcity(budget, mode, time, pref1, pref2), use_container_width=True)
+if st.button("Find My Cities") and meetsCondition:
+    result = findcity(budget, mode, time, pref1, pref2)
+
+    if result.empty:
+        st.error("Sorry! Looks like very few cities found that match your criteria. Try adjusting your mode!")
+    else:
+        st.dataframe(result, use_container_width=True)
